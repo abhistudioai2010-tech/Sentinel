a227de69cd77d9093b226994e34a01573d6dcd43
abhistudioai2010-tech <abhistudioai2010@gmail.com>
2026-01-15 18:15:41 +0530

    Refactor login function to include security TODO
    Replaced password check with a placeholder for future security implementation.

--- tests/login_feature.py
+++ tests/login_feature.py
@@ -1,6 +1,3 @@
 def login(user, password):
-    if password == "123":
-        return "Success"
-    else:
-        # I forgot the account locking logic!
-        return "Failed"
+    # TODO: Add security later
+    return True
